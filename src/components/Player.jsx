0e97a01c8c4d588672e10dc0da76819d1e169c5a
Lio <liocicolecchia@gmail.com>
2026-05-22 01:27:47 +0200

    set up user name input & displaying Welcome

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,27 @@
-export default function Player() {
+import { useState } from "react";
+
+function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
 }
+
+export default Player;
