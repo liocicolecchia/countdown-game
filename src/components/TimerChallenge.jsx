d8a21f858d2b137fbbc9eac17ed22eacbbf23576
Lio <liocicolecchia@gmail.com>
2026-05-25 23:13:42 +0200

    exposing Component API's with useImperativeHandle Hook

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -13,7 +13,7 @@ function TimerChallenge({ title, targetTime }) {
     setTimerStarted(true);
     timer.current = setTimeout(() => {
       setTimerExpired(true);
-      dialog.current.showModal();
+      dialog.current.open();
     }, targetTime * 1000);
   }
 
